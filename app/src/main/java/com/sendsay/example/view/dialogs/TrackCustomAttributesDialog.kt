bc241065d11428083e51a52dbcc8dbd6a2bb6df2
Stas.ProSky <proskuryakov@iprojects.ru>
2025-08-13 18:49:53 +0300

    - Add property management and UI enhancements to TrackCustomAttributes

--- app/src/main/java/com/sendsay/example/view/dialogs/TrackCustomAttributesDialog.kt
+++ app/src/main/java/com/sendsay/example/view/dialogs/TrackCustomAttributesDialog.kt
@@ -3,11 +3,16 @@ package com.sendsay.example.view.dialogs
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Spinner
 import android.widget.TextView
+import androidx.appcompat.widget.SwitchCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.FragmentManager
 import com.sendsay.example.App
@@ -18,7 +23,21 @@ import com.sendsay.sdk.models.PropertiesList
 class TrackCustomAttributesDialog : DialogFragment() {
 
     private lateinit var onUpdate: (PropertiesList) -> Unit
-    private val attributes = HashMap<String, Any>()
+    private val attributes = hashMapOf<String, Any>("cce" to "test-CCE" as Any)
+    private val member_set = mutableMapOf<String, Any>()
+    private val datakey = arrayListOf<List<String>>()
+
+    private val spinnerList = listOf<String>(
+        "set",
+        "update",
+        "insert",
+        "merge",
+        "merge_update",
+        "merge_insert",
+        "push",
+        "unshift",
+        "delete"
+    )
 
     companion object {
         const val TAG = "TrackCustomAttributesDialog"
@@ -42,25 +61,66 @@ class TrackCustomAttributesDialog : DialogFragment() {
     }
 
     private fun initListeners(view: View) {
-        val nameView = view.findViewById(R.id.editTextName) as EditText
-        val valueView = view.findViewById(R.id.editTextValue) as EditText
-        val propertiesView = view.findViewById(R.id.textViewAttributes) as TextView
-        val idsView: TextView = view.findViewById(R.id.idsTextView)
+        val idsRegisteredView: EditText = view.findViewById(R.id.idsTextView)
+        val valTextView: EditText = view.findViewById(R.id.valTextView)
+        val propertiesView: TextView = view.findViewById<TextView>(R.id.textViewAttributes)
 
-        idsView.text = "registered: ${App.instance.registeredIdManager.registeredID}"
+        val keyView: EditText = view.findViewById<EditText>(R.id.editTextPropName)
+        val valueView: EditText = view.findViewById<EditText>(R.id.editTextValue)
+
+        val spinnerMode: Spinner = view.findViewById(R.id.spinner)
+        val switchLayout: LinearLayout = view.findViewById(R.id.switchLayout)
+        val switchWidget: SwitchCompat = view.findViewById(R.id.switchWidget)
+
+        val buttonAdd: Button = view.findViewById<Button>(R.id.buttonAddProperty)
+        val buttonUpdate: Button = view.findViewById<Button>(R.id.buttonUpdate)
+
+        idsRegisteredView.setText(App.instance.registeredIdManager.registeredID)
+
+        val spinnerAdapter: ArrayAdapter<String> =
+            ArrayAdapter(
+                requireContext(),
+                android.R.layout.simple_spinner_dropdown_item,
+                spinnerList
+            )
+        spinnerMode.adapter = spinnerAdapter
+
+        switchLayout.setOnClickListener {
+            switchWidget.isChecked = !switchWidget.isChecked
+        }
 
         propertiesView.text = attributes.asJson()
-        view.findViewById<Button>(R.id.buttonAddAttr).setOnClickListener {
-            if (!nameView.text.isEmpty() && !valueView.text.isEmpty()) {
-                attributes[nameView.text.toString()] = valueView.text.toString()
+
+        buttonAdd.setOnClickListener {
+            if (!keyView.text.isEmpty() && !valueView.text.isEmpty()) {
+                val isCopy = if (switchWidget.isChecked) ".copy" else ""
+                datakey.add(
+                    listOf<String>(
+                        keyView.text.toString(),
+                        "${spinnerMode.selectedItem}$isCopy",
+                        valueView.text.toString()
+                    )
+                )
+                Log.d("DATAKEY", datakey.joinToString())
+                member_set["datakey"] = datakey
+                attributes["member_set"] = member_set
+                Log.d(TAG, attributes.toString())
                 propertiesView.text = attributes.asJson()
             }
         }
+//        buttonAdd.setOnClickListener {
+//            if (!nameView.text.isEmpty() && !valueView.text.isEmpty()) {
+//                attributes[nameView.text.toString()] = valueView.text.toString()
+//                propertiesView.text = attributes.asJson()
+//            }
+//        }
 
-        view.findViewById<Button>(R.id.buttonUpdate).setOnClickListener {
-            val properties = PropertiesList(attributes)
-            onUpdate(properties)
-            dismiss()
+        buttonUpdate.setOnClickListener {
+            if (!idsRegisteredView.text.isEmpty() && !valTextView.text.isEmpty()) {
+                val properties = PropertiesList(attributes)
+                onUpdate(properties)
+                dismiss()
+            }
         }
     }
 }
